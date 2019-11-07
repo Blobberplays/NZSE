8acbcb15b5a213793c7faf9a1c1ebeee04eb16ac
Maciej Krzyszton <stmakrzy@stud.h-da.de>
2019-11-07 15:24:01 +0100

    methoden für Kunde erstellt und namen auf englisch geändert

---
+++ app/src/main/java/com/example/nzse/Client.java
@@ -0,0 +1,23 @@
+package com.example.nzse;
+
+public class Client {
+    boolean agent;
+
+//konstruktor
+    Client(){
+        this.agent=false;
+    }
+
+
+    public boolean Is_agent() {
+        return agent;
+    }
+
+    public void agent(boolean agent) {
+        this.agent = agent;
+    }
+
+
+
+
+}
