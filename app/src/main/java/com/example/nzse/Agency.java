7910218c5bc20b1e432b484d7cd7dcc5f7d232cf
Maciej Krzyszton <stmakrzy@stud.h-da.de>
2019-11-07 15:35:15 +0100

    Agency Klasse erweitert

--- app/src/main/java/com/example/nzse/Agency.java
+++ app/src/main/java/com/example/nzse/Agency.java
@@ -1,4 +1,22 @@
 package com.example.nzse;
-
+import com.example.nzse.Client;
+import com.example.nzse.Immobilie;
+import java.util.ArrayList;
 public class Agency {
+
+    private ArrayList<Client> client_list = new ArrayList<Client>();
+    private ArrayList<Immobilie> immobilie_list = new ArrayList<Immobilie>();
+
+    public ArrayList<Client> getClient_list() {
+        return client_list;
+    }
+
+    public ArrayList<Immobilie> getImmobilie_list() {
+        return immobilie_list;
+    }
+
+    public add_into_arraylist(){
+
+    }
+
 }
