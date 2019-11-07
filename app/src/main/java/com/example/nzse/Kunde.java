c70e80c521993b1a19e88a17e879c8792afccf74
Maciej Krzyszton <stmakrzy@stud.h-da.de>
2019-11-07 15:22:43 +0100

    methoden für Kunde erstellt

--- app/src/main/java/com/example/nzse/Kunde.java
+++ app/src/main/java/com/example/nzse/Kunde.java
@@ -1,6 +1,23 @@
 package com.example.nzse;
 
 public class Kunde {
+    boolean is_agent;
 
-    double is_agent;
+//konstruktor
+    Kunde(){
+        this.is_agent=false;
+    }
+
+
+    public boolean isIs_agent() {
+        return is_agent;
+    }
+
+    public void setIs_agent(boolean is_agent) {
+        this.is_agent = is_agent;
+    }
+
+
+
+
 }
