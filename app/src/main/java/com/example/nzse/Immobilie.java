b7e923107137374336bffaa160d07dd46d1ab8af
Kehr,Christian <christian.kehr@stud.h-da.de>
2019-11-07 15:28:09 +0100

    enum initialisation

--- app/src/main/java/com/example/nzse/Immobilie.java
+++ app/src/main/java/com/example/nzse/Immobilie.java
@@ -1,4 +1,10 @@
 package com.example.nzse;
+import java.lang.String;
 
 public class Immobilie {
+    double price;
+    double rooms_count;
+    boolean buy = false;
+    double provision;
+    String picture;
 }
