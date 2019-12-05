6d92ab7224190e07eaff63d8f7540aa0ecadf795
Kehr,Christian <christian.kehr@stud.h-da.de>
2019-12-05 12:27:06 +0100

    deleted bracket

--- app/src/main/java/com/example/nzse/Kunde.java
+++ app/src/main/java/com/example/nzse/Kunde.java
@@ -1,13 +1,13 @@
 package com.example.nzse;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Kunde extends AppCompatActivity {
     private Button display_search;
