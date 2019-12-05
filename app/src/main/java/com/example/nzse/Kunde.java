9d085d258a78f3b4dbff6854b5025959a4d683a6
Maciej Krzyszton <maciejkrzyszton63@gmail.com>
2019-12-05 12:12:52 +0100

    added seekbar

--- app/src/main/java/com/example/nzse/Kunde.java
+++ app/src/main/java/com/example/nzse/Kunde.java
@@ -6,17 +6,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class Kunde extends AppCompatActivity {
     private Button display_search;
+    private SeekBar price_SeekBar;
+    private TextView seekBarValue;
+
+    final int max = 1000;
+    final int min = 1;
+    final int avg = (max-min)/2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kunde);
 
-        display_search = (Button)findViewById(R.id.button_list_immobilien);
+        display_search = findViewById(R.id.button_list_immobilien);
+        price_SeekBar = findViewById(R.id.simpleSeekBar);
+        seekBarValue = findViewById(R.id.display_price);
+
 
         display_search.setOnClickListener(new View.OnClickListener() {
 
@@ -27,13 +37,39 @@ public class Kunde extends AppCompatActivity {
         });
 
 
+        seekbarSet();
     }
-    void openScreenSearch(){
-        Agency a =(Agency)getIntent().getSerializableExtra("Agency");
+
+
+
+
+    void openScreenSearch() {
+        Agency a = (Agency) getIntent().getSerializableExtra("Agency");
         Intent intend = new Intent(this, Search.class).
                 putExtra("test_string", "das kommt von Kunde").
-                putExtra("Agency",getIntent().getSerializableExtra("Agency"));
+                putExtra("Agency", getIntent().getSerializableExtra("Agency"));
 
         startActivity(intend);
-    };
+    }
+    void seekbarSet(){
+        seekBarValue.setText(String.valueOf(min+avg));
+        price_SeekBar.setMax(max-min);
+        price_SeekBar.setProgress(min+avg);
+        price_SeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress,
+                                          boolean fromUser) {
+                // TODO Auto-generated method stub
+                seekBarValue.setText(String.valueOf(min+progress));
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+
+        });
+    }
+
+
 }
